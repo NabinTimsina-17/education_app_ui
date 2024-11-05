44dfe82971c18623bb34b0b912f0653a0630d9e1
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2024-11-05 10:52:52 +0545

    .

---
+++ lib/home_screen.dart
@@ -0,0 +1,198 @@
+import 'package:education_app_ui/Utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 45),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    "Images/menu1.png",
+                    width: 40,
+                    height: 40,
+                  ),
+                  Image.asset(
+                    "Images/search1.png",
+                    height: 40,
+                    width: 40,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          const Text(
+                            "Hi Julia",
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Image.asset(
+                            "Images/hand.png",
+                            height: 30,
+                            width: 30,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const Text(
+                        "Today is a good day\nto learn something new!",
+                        style: TextStyle(color: Colors.black54, fontSize: 17),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  Image.asset(
+                    "Images/profile.png",
+                    height: 110,
+                    width: 110,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Card(
+                    color: primaryColor,
+                    child: const SizedBox(
+                      height: 50,
+                      width: 90,
+                      child: Center(
+                        child: Text(
+                          "Top",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Text(
+                    "Design",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const Text(
+                    "Marketing",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Image.asset(
+                    "Images/filter.png",
+                    height: 40,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Categories",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text(
+                    "See All",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 595,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.white, Color.fromARGB(255, 233, 236, 246)],
+                ),
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 0,
+                    child: displayImage(250, "Images/graphic design.png"),
+                  ),
+                  Positioned(
+                    right: 0,
+                    child: displayImage(220, "Images/programming.png"),
+                  ),
+                  Positioned(
+                    left: 0,
+                    top: 260,
+                    child: displayImage(220, "Images/financee.png"),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
+
+Padding displayImage(double heigh, image) {
+  return Padding(
+    padding: const EdgeInsets.only(left: 15, right: 15),
+    child: Expanded(
+        child: SizedBox(
+      height: heigh,
+      width: 193,
+      child: Image.asset(
+        image,
+        fit: BoxFit.fill,
+      ),
+    )),
+  );
+}
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'package:education_app_ui/Utils/colors.dart';
+import 'package:education_app_ui/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -33,11 +35,118 @@ class SplashScreen extends StatelessWidget {
                 height: MediaQuery.of(context).size.height / 2.88,
                 width: MediaQuery.of(context).size.width,
                 decoration: const BoxDecoration(
-                  image: DecorationImage(image:    
-                   AssetImage('Images/image.png'), 
-                   fit: BoxFit.fill),
-                )
+                  image: DecorationImage(
+                      image: AssetImage('Images/image.png'), fit: BoxFit.cover),
+                ),
+              ),
             ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 2.88,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(70),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 50),
+                  child: Column(
+                    children: [
+                      const Text('Learning Everywhere',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "Learn with pleasure with \n us where you are ",
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(top: 60, left: 15, right: 15),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 16,
+                              width: 16,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(200),
+                                border: Border.all(
+                                  width: 1.5,
+                                  color: primaryColor,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 16,
+                            ),
+                            Container(
+                              height: 16,
+                              width: 16,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(200),
+                                border: Border.all(
+                                  width: 1.5, 
+                                  color: primaryColor
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 16,
+                            ),
+                            Container(
+                              height: 16,
+                              width: 45,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(200),
+                                color: primaryColor
+                              ),
+                            ),
+                            const Spacer(),
+                            GestureDetector(
+                              // for navigation
+                              onTap: () {
+                              Navigator.push(context, 
+                              MaterialPageRoute(builder: (context) => const HomeScreen()
+                              )
+                              );
+                              },
+                              child: Container(
+                                height: 75,
+                                width: 150,
+                                decoration: 
+                                BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: primaryColor
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    'Get Started',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
